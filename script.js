1c04bef493087cb6dc8ef5598bf87415d8a33303
waalnaabi-code <wa.alnaabi@icloud.com>
2026-07-07 15:02:11 +0400

    Refactor cart management and loadCart function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 
+
 function addToCart(name, price){
 
 cart.push({
@@ -11,9 +12,116 @@ price:price
 });
 
 
-localStorage.setItem("cart",JSON.stringify(cart));
+localStorage.setItem(
+"cart",
+JSON.stringify(cart)
+);
 
 
 alert("تم إضافة المنتج للسلة 🛒");
 
 }
+
+
+
+
+function loadCart(){
+
+let box = document.getElementById("cartItems");
+
+let totalBox = document.getElementById("total");
+
+
+if(!box) return;
+
+
+let html="";
+let total=0;
+
+
+
+cart.forEach((item,index)=>{
+
+
+html += `
+
+<div class="cartItem">
+
+<h3>
+${item.name}
+</h3>
+
+
+<p>
+${item.price} OMR
+</p>
+
+
+<button onclick="removeItem(${index})">
+حذف
+</button>
+
+
+</div>
+
+`;
+
+
+total += item.price;
+
+
+});
+
+
+
+box.innerHTML = html;
+
+
+totalBox.innerHTML = total;
+
+
+
+let message="طلب جديد من Vura:\n\n";
+
+
+cart.forEach(item=>{
+
+message +=
+item.name+" - "+item.price+" OMR\n";
+
+});
+
+
+message +=
+"\nالمجموع: "+total+" OMR";
+
+
+
+document.getElementById("whatsapp").href =
+"https://wa.me/96876757295?text="
++
+encodeURIComponent(message);
+
+
+}
+
+
+
+function removeItem(index){
+
+cart.splice(index,1);
+
+
+localStorage.setItem(
+"cart",
+JSON.stringify(cart)
+);
+
+
+loadCart();
+
+}
+
+
+
+loadCart();
